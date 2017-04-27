936a23db3c32d998e0d8f23afa3dc01ddf823c04
KolyaAlavackii <nikolayalavackii@gmail.com>
2017-04-27 10:50:04 +0300

    refactoring

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-class App extends React.Component {
-   render() {
-       return (
-           <MuiThemeProvider>
-                <div>
-                    {this.props.children}
-                </div>
-           </MuiThemeProvider>
-       );
-   }
+const App = (props) => {
+    return (
+        <MuiThemeProvider>
+            <div>
+                {props.children}
+            </div>
+        </MuiThemeProvider>
+    );
 }
 
 export default App;
